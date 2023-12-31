c6fa1e6b834d3f6779d5e78879c5cb9a927fb251
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-12-31 14:37:27 +0100

    Installing flask with puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
 #installing Flask from pip3
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => pip3,
+class flask {
+  package { 'flask':
+    ensure => 'installed',
+    provider => 'pip3',
+  }
 }
