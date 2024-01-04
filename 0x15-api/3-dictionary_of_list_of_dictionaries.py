9b8fadf5fe689a5562066f5cf2201349e34603fb
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2024-01-04 12:29:44 +0100

    finished final task

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,31 +9,24 @@ import urllib.request
 
 def to_json():
     """ Request Function """
-    emp_todo ={}
+    emp_todo = {}
     todo_url = "https://jsonplaceholder.typicode.com/todos/"
     user_url = "https://jsonplaceholder.typicode.com/users/"
-    e_nam = ""
-    rows = {}
     row = {}
     with urllib.request.urlopen(todo_url) as response:
         todo_json = json.loads(response.read())
     with urllib.request.urlopen(user_url) as res:
         user_json = json.loads(res.read())
-    for user in user_json:
-        for todo in todos:
-            if user['id'] = todo['userId']:
-                emp_todo[user['id']] = todo
-    """for todos in todo_json:
-        emp_todo['userId'].append(todos)
-    rows[str(id)] = []
-    for tod in emp_todo:
-        row["task"] = tod['title']
-        row["completed"] = tod['completed']
-        row["username"] = user_json['username']
-        rows[str(id)].append(row)
-    json_ = json.dumps(rows)
+    for todos in todo_json:
+        emp_todo[todos['userId']] = []
+        for user in user_json:
+            row["task"] = todos['title']
+            row["completed"] = todos['completed']
+            row["username"] = user['username']
+            emp_todo[todos['userId']].append(row)
+    json_ = json.dumps(emp_todo)
     with open("todo_all_employees.json", 'w') as file:
-        file.write(json_)"""
+        file.write(json_)
 
 
 if __name__ == "__main__":
