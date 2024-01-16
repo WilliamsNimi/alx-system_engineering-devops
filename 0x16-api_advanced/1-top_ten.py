cf3fce33204110aa7a83e716947b618058512468
WilliamsNimi <williamson.nimi@gmail.com>
2024-01-16 22:10:33 +0100

    Not allowing Redirects

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,7 +10,7 @@ def top_ten(subreddit):
     red_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent":"new_user"}
     params = {"limit":10}
-    response = requests.get(red_url, headers=headers, params=params)
+    response = requests.get(red_url, headers=headers, params=params, allow_redirects=False)
     if response.status_code == 200:
         content = response.json().get('data').get('children')
         titles = list(map(lambda x: x.get('data').get('title'), content))
@@ -21,4 +21,3 @@ def top_ten(subreddit):
 
 if __name__ == "__main__":
     top_ten(sys.argv[1])
-    
