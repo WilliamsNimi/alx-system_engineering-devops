dee5062614463b02085e23b8e2a54d9e60b11fc4
WilliamsNimi <williamson.nimi@gmail.com>
2024-01-16 21:57:41 +0100

    Printing top_ten from subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Fetching data from the Reddit API """
+
+
+import sys
+import requests
+
+def top_ten(subreddit):
+    red_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent":"new_user"}
+    params = {"limit":10}
+    response = requests.get(red_url, headers=headers, params=params)
+    if response.status_code == 200:
+        content = response.json().get('data').get('children')
+        titles = list(map(lambda x: x.get('data').get('title'), content))
+        for title in titles:
+            print(title)
+    else:
+        print(None)
+
+if __name__ == "__main__":
+    top_ten(sys.argv[1])
