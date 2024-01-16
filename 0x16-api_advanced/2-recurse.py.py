d0dabb689b0df8a95a294cc2adb154148f15b223
WilliamsNimi <williamson.nimi@gmail.com>
2024-01-16 22:23:41 +0100

    recursive Requests

---
+++ 0x16-api_advanced/2-recurse.py.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Fetching data from the Reddit API """
+
+
+import requests
+import sys
+
+def recurse(subreddit, hot_list=[], after=""):
+    """ Top 10 titles in Subreddits"""
+    red_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent":"new_user"}
+    params = {"after": after}
+    response = requests.get(red_url, headers=headers, params=params, allow_redirects=False)
+    if response.status_code == 200:
+        content = response.json().get('data').get('children')
+        content2 = response.json().get('data').get('after')
+        titles = list(map(lambda x: x.get('data').get('title'), content))
+        hot_list.extend(titles)
+        if content2 is None:
+            return hot_list
+        return recurse(subreddit, hot_list, content2)
+    else:
+        print(None)
+
+if __name__ == "__main__":
+    recurse(sys.argv[1])
